bf00fb4fe52b85cd8ad8bcdcee432223384abac3
six519 <ferdinandsilva@ferdinandsilva.com>
2023-07-03 12:17:39 +0800

    django 4.2.2 compatibility

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bootstrap-pagination',
-    version='1.7.1',
+    version='1.7.2',
     keywords="django bootstrap pagination templatetag",
     author=u'Jason McClellan<jason@jasonmcclellan.io>, Koert van der Veer<koert@ondergetekende.nl>',
     author_email='jason@jasonmccllelan.io',
